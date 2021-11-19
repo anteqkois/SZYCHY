52fc116acbceecc1c479607b8ccddfe1504e1b18
unknown <micha.ogorek@op.pl>
2021-11-19 23:26:45 +0100

    completeHeader

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import MainLayout from './layouts/MainLayout';
 import ScrollToTop from './components/utils/ScrollToTop';
 import NotFound from './components/utils/NotFound';
 import Login from './components/Login/Login';
+import Main from './pages/Main';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -39,6 +40,7 @@ function App() {
             <Switch>
               {/* <PrivateRoute path="/admin" component={<h1>Admin</h1>} /> */}
               <Route exact path="/" component={Login} />
+              <Route exact path="/main" component={Main}/>
               {/* <Route path="*" component={NotFound} /> */}
             </Switch>
           </MainLayout>
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import Header from "../containers/header/Header";
+import Header from "../containers/header/Header";
 import Navbar from "../containers/navigation/Navigation";
 
 
@@ -7,7 +7,7 @@ function Main(){
     return(
         <div>
             <Navbar></Navbar>
-            {/* <Header></Header> */}
+            <Header></Header>
         </div>
     );
 }
