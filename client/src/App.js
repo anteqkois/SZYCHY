782b7cc484773d0006b5c88a334bc38012770bca
Wojciech <kowalczyk.wojtek168@gmail.com>
2021-11-19 22:43:18 +0100

    abc

--- client/src/App.js
+++ client/src/App.js
@@ -12,12 +12,7 @@ import { ErrorProvider } from './providers/ErrorContext';
 import MainLayout from './layouts/MainLayout';
 import ScrollToTop from './components/utils/ScrollToTop';
 import NotFound from './components/utils/NotFound';
-<<<<<<< HEAD
-import Test from './components/Test';
-import Main from "./pages/Main";
-=======
 import Login from './components/Login/Login';
->>>>>>> 986007d82dd30093504171ec63f147f4f7d5fad4
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -42,16 +37,10 @@ function App() {
           <MainLayout>
             <ScrollToTop />
             <Switch>
-<<<<<<< HEAD
-              <PrivateRoute path="/admin" component={<h1>Admin</h1>} />
-              {/* <Route exact path="/" component={Test} /> */}
-              <Route exact path="/" component={Main} />
-              <Route path="*" component={NotFound} />
-=======
               {/* <PrivateRoute path="/admin" component={<h1>Admin</h1>} /> */}
               <Route exact path="/" component={Login} />
               {/* <Route path="*" component={NotFound} /> */}
->>>>>>> 986007d82dd30093504171ec63f147f4f7d5fad4
+              <Route exact path="/add" component={Add} />
             </Switch>
           </MainLayout>
         </ErrorProvider>
