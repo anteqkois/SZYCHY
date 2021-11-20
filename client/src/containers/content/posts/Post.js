f8085a6f1934294efa887227d8fcf67781d90aea
unknown <micha.ogorek@op.pl>
2021-11-20 04:17:01 +0100

    changeDB

--- client/src/containers/content/posts/Post.js
+++ client/src/containers/content/posts/Post.js
@@ -4,13 +4,13 @@ import Title from "./Title";
 import Category from "./Category";
 import styled from "styled-components";
 import User from "./User";
+import Answer from "./Answer";
 
 const StyledPost = styled.div`
     background:#222222;
     width:70%;
     margin:40px auto;
-    padding:15px;
-    padding-bottom:50px;
+    padding:30px 15px 50px;
     border: none;
     outline: none;
     box-shadow: 0 2px 4px 0 #3700B3;
@@ -23,6 +23,7 @@ function Post(props){
             <Title title={props.title}></Title>
             <ContentPost contentPost={props.contentPost}></ContentPost>
             <User user={props.user}></User>
+            <Answer/>
         </StyledPost>
     );
 }
